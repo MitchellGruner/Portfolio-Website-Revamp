353e9d7fb80a19fa7f9d32e26c08ebc0dd9441b1
Mitchell Gruner <mit.gruner@gmail.com>
2023-05-04 17:33:51 -0500

    initial commit of Projects.js

---
+++ portfolio-website-revamp/src/components/Projects.js
@@ -0,0 +1,9 @@
+const Projects = () => {
+    return (
+        <div id="projects" className="min-h-screen">
+            <h1 className="text-4xl text-white">Projects</h1>
+        </div>
+    );
+}
+
+export default Projects;
