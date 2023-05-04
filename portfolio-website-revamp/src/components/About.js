69761731187a406b2a46a2105c465f43cd88d97f
Mitchell Gruner <mit.gruner@gmail.com>
2023-05-04 17:33:33 -0500

    initial commit of About.js

---
+++ portfolio-website-revamp/src/components/About.js
@@ -0,0 +1,9 @@
+const About = () => {
+    return (
+        <div id="about" className="min-h-screen">
+            <h1 className="text-4xl text-white">About</h1>
+        </div>
+    );
+}
+
+export default About;
