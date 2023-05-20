175cf952a7f4deb718b6f316ed98980e1db24b13
Mitchell Gruner <mit.gruner@gmail.com>
2023-05-20 15:11:50 -0500

    rename file to Projects.tsx

--- portfolio-website-revamp/src/components/Projects.js
+++
@@ -1,9 +0,0 @@
-const Projects = () => {
-    return (
-        <div id="projects" className="min-h-screen">
-            <h1 className="text-4xl text-white">Projects</h1>
-        </div>
-    );
-}
-
-export default Projects;
