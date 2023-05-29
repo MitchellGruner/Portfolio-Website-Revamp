eea2d7040a05acd3fca2d01c77b86d3df8c81a9d
Mitchell Gruner <mit.gruner@gmail.com>
2023-05-29 15:36:16 -0500

    dynamically load info from json file

--- portfolio-website-revamp/src/components/Projects.tsx
+++ portfolio-website-revamp/src/components/Projects.tsx
@@ -1,28 +1,20 @@
 import React from "react";
-import Image, { StaticImageData } from "next/image";
-
-import QuizList from "../../public/static/images/quizlist1.png";
-import CloudVend from "../../public/static/images/cloudvend1.png";
-import MobileLearningApp from "../../public/static/images/mobile-learning-app.png";
-import BaseballAggressiveAnalytics from "../../public/static/images/baseball-aggressive-analytics.png";
-import Hangman from "../../public/static/images/hangman.png";
-import ThePath from "../../public/static/images/thepath.png";
 
 function ProjectLogo({
+    id,
     logo,
     alt,
-    link
 }: {
-    logo: StaticImageData;
+    id: number;
+    logo: string;
     alt: string;
-    link: string;
 }) {
     return (
         <div className="project-logo-container mx-auto mb-6 xs:mb-8 md:mb-8 lg:mb-10">
-            <a href={link} className="flex mx-12 xs:mx-4 sm:mx-10 md:mx-6 lg:mx-4">
-                <Image
+            <a href={`/projects/${id}`} className="flex mx-12 xs:mx-4 sm:mx-10 md:mx-6 lg:mx-4">
+                <img
                     className="mx-auto"
-                    src={logo}
+                    src={`/static/images/${logo}`}
                     alt={alt}
                 />
             </a>
@@ -30,7 +22,8 @@ function ProjectLogo({
     )
 }
 
-const Projects = () => {
+const Projects = (props) => {
+    console.log("props", props);
     return (
         <div id="projects" className="h-auto mt-2 mb-16 xs:mb-20 sm:mb-24 md:mb-32 md:mt-2 lg:mb-36 xl:mb-40 2xl:mb-44">
             <div className="text-center mx-4 sm:mx-32 md:mx-42 lg:mx-56 xl:mx-80">
@@ -39,12 +32,16 @@ const Projects = () => {
             </div>
 
             <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 xs:gap-2 sm:gap-4 lg:gap-6 xs:mx-4 sm:mx-6 md:mx-8 lg:mx-32 xl:mx-36">
-                <ProjectLogo logo={QuizList} alt="QuizList Logo" link="/projects/quiz-list" />
-                <ProjectLogo logo={CloudVend} alt="CloudVend Logo" link="/projects/cloud-vend" />
-                <ProjectLogo logo={MobileLearningApp} alt="Mobile Learning App Logo" link="/projects/mobile-learning-app" />
-                <ProjectLogo logo={ThePath} alt="The Path Logo" link="/projects/the-path" />
-                <ProjectLogo logo={Hangman} alt="Hangman Logo" link="/projects/hangman" />
-                <ProjectLogo logo={BaseballAggressiveAnalytics} alt="Baseball Aggressive Analytics Logo" link="/projects/baseball-aggressive-analytics" />
+                {props.productDetails.productDetails.map((project) => {
+                    return (
+                        <ProjectLogo
+                            key={project.id}
+                            id={project.id}
+                            logo={project.image}
+                            alt={project.alt}
+                        />
+                    )
+                })}
             </div>
         </div>
     );
