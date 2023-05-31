5679e13b419315112fd290d02b5d59e118326214
Mitchell Gruner <mit.gruner@gmail.com>
2023-05-30 22:13:19 -0500

    import "ProjectsInfo.scss"

--- portfolio-website-revamp/src/pages/_app.js
+++ portfolio-website-revamp/src/pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/About.scss';
 import '../styles/globals.scss';
 import '../styles/Navbar.scss';
 import '../styles/Projects.scss';
+import '../styles/ProjectsInfo.scss';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
