dc61e40832f55d8e1f1661b100ada38d8ae57530
Mitchell Gruner <mit.gruner@gmail.com>
2023-06-04 18:36:10 -0500

    add div with id of 'projects-section' for offset

--- portfolio-website-revamp/src/components/Projects.tsx
+++ portfolio-website-revamp/src/components/Projects.tsx
@@ -29,6 +29,7 @@ const Projects = (props) => {
                 <h2 className="text-3xl xs:text-4xl md:text-5xl 2xl:text-6xl font-bold text-white">Here are some <span className="text-black font-extrabold">highlights</span>...</h2>
                 <p className="text-md p-4 mb-6 xs:mb-10 2xl:mt-6 2xl:mb-14 xs:text-lg md:text-xl 2xl:text-2xl text-white">From shopping in an ecommerce full-stack application, to analyzing trends with statistical analysis in sports, to playing trivia with a wide range of interesting and fun questions, these projects offer a lit bit of everything.</p>
             </div>
+            <div id="projects-section"></div>
 
             <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 xs:gap-2 sm:gap-4 lg:gap-6 xs:mx-4 sm:mx-6 md:mx-8 lg:mx-32 xl:mx-36">
                 {props.productDetails.productDetails.map((project) => {
