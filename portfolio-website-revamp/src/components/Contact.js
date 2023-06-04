4578551ba6686225b7f119581b0352d8ab2d1e90
Mitchell Gruner <mit.gruner@gmail.com>
2023-06-04 18:35:37 -0500

    add div with id of "contact-section" for offset

--- portfolio-website-revamp/src/components/Contact.js
+++ portfolio-website-revamp/src/components/Contact.js
@@ -1,6 +1,7 @@
 const Contact = () => {
     return (
         <div id="contact" className="min-h-screen">
+            <div id="contact-section"></div>
             <h1 className="text-4xl text-white">Contact</h1>
         </div>
     );
