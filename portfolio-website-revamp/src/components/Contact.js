2385d5b04026c9ed491e7f9999ea307848cbcfbf
Mitchell Gruner <mit.gruner@gmail.com>
2023-06-10 12:45:52 -0500

    create form based on 'https://github.com/nikitapryymak'

--- portfolio-website-revamp/src/components/Contact.js
+++ portfolio-website-revamp/src/components/Contact.js
@@ -1,10 +1,150 @@
-const Contact = () => {
+import {
+    Button,
+    Container,
+    FormControl,
+    FormErrorMessage,
+    FormLabel,
+    Heading,
+    Input,
+    Text,
+    Textarea,
+    useToast,
+  } from "@chakra-ui/react";
+  import { useState } from "react";
+  import { sendContactForm } from "../lib/api";
+  
+  const initValues = { name: "", email: "", subject: "", message: "" };
+  
+  const initState = { isLoading: false, error: "", values: initValues };
+  
+  export default function Home() {
+    const toast = useToast();
+    const [state, setState] = useState(initState);
+    const [touched, setTouched] = useState({});
+  
+    const { values, isLoading, error } = state;
+  
+    const onBlur = ({ target }) =>
+      setTouched((prev) => ({ ...prev, [target.name]: true }));
+  
+    const handleChange = ({ target }) =>
+      setState((prev) => ({
+        ...prev,
+        values: {
+          ...prev.values,
+          [target.name]: target.value,
+        },
+      }));
+  
+    const onSubmit = async () => {
+      setState((prev) => ({
+        ...prev,
+        isLoading: true,
+      }));
+      try {
+        await sendContactForm(values);
+        setTouched({});
+        setState(initState);
+        toast({
+          title: "Message sent.",
+          status: "success",
+          duration: 2000,
+          position: "top",
+        });
+      } catch (error) {
+        setState((prev) => ({
+          ...prev,
+          isLoading: false,
+          error: error.message,
+        }));
+      }
+    };
+  
     return (
-        <div id="contact" className="min-h-screen">
-            <div id="contact-section"></div>
-            <h1 className="text-4xl text-white">Contact</h1>
+      <Container maxW="450px" mt={12}>
+        <div className="text-center mx-4 sm:mx-32 md:mx-42 lg:mx-56 xl:mx-80">
+            <h2 className="text-3xl xs:text-4xl md:text-5xl 2xl:text-6xl font-bold text-white">Let's Get <span className="text-black font-extrabold">Connected</span>...</h2>
+            <p className="text-md p-4 mb-6 xs:mb-10 2xl:mt-6 2xl:mb-14 xs:text-lg md:text-xl 2xl:text-2xl text-white">I am always looking for the next greatest opportunity to bring my web development and design skills to the next level. Whether you have a question or just want to say 'hi', my inbox is always open. Like what you see? Reach out to me today and let's do this!</p>
         </div>
+        <Heading>Contact</Heading>
+        {error && (
+          <Text color="red.300" my={4} fontSize="xl">
+            {error}
+          </Text>
+        )}
+  
+        <FormControl isRequired isInvalid={touched.name && !values.name} mb={5}>
+          <FormLabel>Name</FormLabel>
+          <Input
+            type="text"
+            name="name"
+            errorBorderColor="red.300"
+            value={values.name}
+            onChange={handleChange}
+            onBlur={onBlur}
+          />
+          <FormErrorMessage>Required</FormErrorMessage>
+        </FormControl>
+  
+        <FormControl isRequired isInvalid={touched.email && !values.email} mb={5}>
+          <FormLabel>Email</FormLabel>
+          <Input
+            type="email"
+            name="email"
+            errorBorderColor="red.300"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={onBlur}
+          />
+          <FormErrorMessage>Required</FormErrorMessage>
+        </FormControl>
+  
+        <FormControl
+          mb={5}
+          isRequired
+          isInvalid={touched.subject && !values.subject}
+        >
+          <FormLabel>Subject</FormLabel>
+          <Input
+            type="text"
+            name="subject"
+            errorBorderColor="red.300"
+            value={values.subject}
+            onChange={handleChange}
+            onBlur={onBlur}
+          />
+          <FormErrorMessage>Required</FormErrorMessage>
+        </FormControl>
+  
+        <FormControl
+          isRequired
+          isInvalid={touched.message && !values.message}
+          mb={5}
+        >
+          <FormLabel>Message</FormLabel>
+          <Textarea
+            type="text"
+            name="message"
+            rows={4}
+            errorBorderColor="red.300"
+            value={values.message}
+            onChange={handleChange}
+            onBlur={onBlur}
+          />
+          <FormErrorMessage>Required</FormErrorMessage>
+        </FormControl>
+  
+        <Button
+          variant="outline"
+          colorScheme="blue"
+          isLoading={isLoading}
+          disabled={
+            !values.name || !values.email || !values.subject || !values.message
+          }
+          onClick={onSubmit}
+        >
+          Submit
+        </Button>
+      </Container>
     );
-}
-
-export default Contact;
+  }
