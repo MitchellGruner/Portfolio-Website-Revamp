be4ee801da0b9bf7cc2f21b04b7da4a8dd794a3f
Mitchell Gruner <mit.gruner@gmail.com>
2023-07-04 14:49:04 -0500

    suppressHydrationWarning for Html

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" suppressHydrationWarning>
       <Head />
       <link rel="shortcut icon" href="/favicon/favicon.ico?v=1" />
       <body>
