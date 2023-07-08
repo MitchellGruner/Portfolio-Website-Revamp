5de87cac34dd2c26c124b2b0811cf8081956233d
Mitchell Gruner <mit.gruner@gmail.com>
2023-07-08 12:53:28 -0500

    change '?v=2' to '?v=3' for Contact.scss import

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,7 @@ import '../styles/globals.scss?v=2';
 import '../styles/Navbar.scss?v=2';
 import '../styles/Projects.scss?v=2';
 import '../styles/ProjectsInfo.scss?v=2';
-import '../styles/Contact.scss?v=2';
+import '../styles/Contact.scss?v=3';
 
 export default function App({ Component, pageProps }) {
   return (
