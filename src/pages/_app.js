c89ffa4d788de2a920acf0414e6dffe0747ebc8c
Mitchell Gruner <mit.gruner@gmail.com>
2023-07-08 12:27:05 -0500

    change '?v=1' to '?v=2'

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,11 @@
 import Layout from '../components/Layout';
 
-import '../styles/About.scss?v=1';
-import '../styles/globals.scss?v=1';
-import '../styles/Navbar.scss?v=1';
-import '../styles/Projects.scss?v=1';
-import '../styles/ProjectsInfo.scss?v=1';
-import '../styles/Contact.scss?v=1';
+import '../styles/About.scss?v=2';
+import '../styles/globals.scss?v=2';
+import '../styles/Navbar.scss?v=2';
+import '../styles/Projects.scss?v=2';
+import '../styles/ProjectsInfo.scss?v=2';
+import '../styles/Contact.scss?v=2';
 
 export default function App({ Component, pageProps }) {
   return (
