fb77f4f6f5ebbd0b0cadd3471823e0d30325a0c9
Mitchell Gruner <mit.gruner@gmail.com>
2024-02-15 20:51:15 -0600

    update text to reflect current position at work

--- src/components/WorkProjects.tsx
+++ src/components/WorkProjects.tsx
@@ -33,7 +33,7 @@ const WorkProjects = (props) => {
         <div id="projects" className="h-auto mt-2 mb-12 xs:mb-16 sm:mb-20 md:mb-24 lg:mb-28 xl:mb-32 2xl:mb-36">
             <div className="text-center mt-10 mb-4 sm:mx-32 md:mx-42 lg:mx-56 xl:mx-80">
                 <h2 className="text-3xl xs:text-4xl md:text-5xl 2xl:text-6xl font-bold text-white">Work <span className="text-black font-extrabold">Projects</span></h2>
-                <p className="text-md p-4 mb-6 xs:mb-10 2xl:mt-6 2xl:mb-14 xs:text-lg md:text-xl 2xl:text-2xl text-white">After nearly two years of professional work experience as a front end developer at Electronic Express, I have implemented several key features to the website. This includes the hamburger menu, an overhaul of the product details page card (shown below), and several brand pages for various companies; these were built using HTML and CSS, CSS Animations, JavaScript, jQuery, and Bootstrap as a CSS framework.</p>
+                <p className="text-md p-4 mb-6 xs:mb-10 2xl:mt-6 2xl:mb-14 xs:text-lg md:text-xl 2xl:text-2xl text-white">After two and a half years of professional work experience as a front end developer at Electronic Express, I have implemented several key features to the website. Some major features would include: the hamburger menu, an overhaul of the cart page and product details card, and several landing pages for leading companies - These were built using HTML and CSS, CSS Animations, JavaScript, jQuery, and Bootstrap as a CSS framework.</p>
             </div>
 
             <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 xs:gap-2 sm:gap-4 lg:gap-6 xs:mx-4 sm:mx-6 md:mx-8 lg:mx-32 xl:mx-36">
