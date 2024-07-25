6a928d8b5eaf3749c18bc5731d95b48076a58bd7
Mitchell Gruner <mit.gruner@gmail.com>
2024-07-25 18:24:47 -0500

    add <p> to title and change margin
    this paragraph tag will allow us to have the 'swoosh' effect on the title

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -31,8 +31,8 @@ function ProjectLogo({
 const Projects = (props) => {
     return (
         <div id="projects" className="h-auto mt-2 mb-12 xs:mb-16 sm:mb-20 md:mb-24 lg:mb-28 xl:mb-32 2xl:mb-36">
-            <div className="text-center mx-4 sm:mx-32 md:mx-42 lg:mx-56 xl:mx-80">
-                <h2 className="text-3xl xs:text-4xl md:text-5xl 2xl:text-6xl font-bold text-white">Here are some <span className="text-black font-extrabold">highlights</span>...</h2>
+            <div className="text-center sm:mx-20 md:mx-42 xl:mx-64">
+                <h2 className="text-3xl xs:text-4xl md:text-5xl 2xl:text-6xl font-bold text-white"><p>Here are some <span className="text-black font-extrabold">highlights</span>...</p></h2>
                 <p className="text-md p-4 mb-6 xs:mb-10 2xl:mt-6 2xl:mb-14 xs:text-lg md:text-xl 2xl:text-2xl text-white">From a dog and cat sitting UI/UX design project, to playing trivia with a wide range of interesting and fun questions, to a full-stack web application for a fictional restuarant that utilizes an API, these projects offer a <span className="italic">little bit of everything</span> for everyone.</p>
             </div>
             <div id="projects-section"></div>
