182613baca5d803958115bdf10f87556053d9b05
Mitchell Gruner <mit.gruner@gmail.com>
2025-01-11 18:59:09 -0600

    remove bolded text of title on home page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -105,7 +105,7 @@ const Contact = () => {
   return (
     <div id="contact" className="h-auto mt-2 mb-8">
       <div className="text-center">
-          <h2 className="text-3xl xs:text-4xl md:text-5xl 2xl:text-6xl font-bold text-white"><p>Let&apos;s Get <span className="text-black font-extrabold">Connected</span>...</p></h2>
+          <h2 className="text-3xl xs:text-4xl md:text-5xl 2xl:text-6xl font-bold text-white"><p>Let&apos;s Get Connected...</p></h2>
           <p className="p-4 mb-6 xs:mb-10 2xl:mt-6 2xl:mb-14 xs:text-lg md:text-xl 2xl:text-2xl text-white">I am always on the lookout for the next greatest opportunity to bring my web development and design skills to the next level. Whether you have a question or just want to say &apos;hi&apos;, my inbox is always open. Like what you see? Reach out to me today and let&apos;s do this!</p>
       </div>
       <div id="contact-section"></div>
