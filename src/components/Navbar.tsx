645178343edb186c19107fc766ac4bae20f3353c
Mitchell Gruner <mit.gruner@gmail.com>
2025-01-12 13:10:38 -0600

    use one consistent header across all pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,18 +5,6 @@ import Image from "next/image";
 import {Link} from "react-scroll";
 import {SiCodeberg} from "react-icons/si";
 
-if (typeof window !== 'undefined' && typeof document !== "undefined") {
-    const hostname = window.location.href.indexOf("/projects") > -1;
-    
-    if (hostname) {
-        document.getElementById("nav-projects").style.display = "block";
-        document.getElementById("nav-root").style.display = "none";
-    } else {
-        document.getElementById("nav-projects").style.display = "none";
-        document.getElementById("nav-root").style.display = "block";
-    }
-}
-
 function NavLinksRoot({
     link,
     title,
@@ -195,8 +183,8 @@ const Navbar = () => {
                     <a href="/blog" className="block text-xl xs:text-2xl">Blog</a>
                     </div>
                 </div>
-                <div className="2xl:max-w-screen-2xl m-auto desktop-nav">
-                    <nav className="h-20 flex justify-between p-6 md:px-8 lg:px-10 xl:px-16 2xl:px-14">
+                <div className="2xl:max-w-screen-2xl w-full m-auto desktop-nav">
+                    <nav className="h-20 w-full flex justify-between p-6 md:px-8 lg:px-10 xl:px-16 2xl:px-14">
                         <div className="flex items-center">
                             <h1 className="mr-2 md:mr-4">
                                 <Link to="about" spy={true} smooth={true} offset={-100} duration={500} className="block text-xl md:text-2xl 2xl:text-3xl" onClick={updateMenuRoot}>
@@ -209,43 +197,6 @@ const Navbar = () => {
                             <NavLinksRoot link="projects" title="Projects" scroll={true} />
                             <NavLinksRoot link="contact" title="Contact" scroll={true} />
                             <NavLinksRoot link="/blog" title="Blog" scroll={false} />
-                        </ul>
-                    </nav>
-                </div>
-            </nav>
-
-            {/* navbar for when user is on /projects/* pages */}
-            <nav id="nav-projects" className="w-full contents justify-between">
-                <div className="mobile-nav w-full h-auto">
-                    <nav className="w-full h-20 bg-black flex justify-between items-center p-4 xs:pl-6">
-                        <div className="flex items-center">
-                            <h1 className="text-xl xs:text-2xl"><a href="https://mitchellgruner.com" className="block text-xl md:text-2xl 2xl:text-3xl">Mitchell Gruner</a></h1>
-                            <SiCodeberg className="icon ml-2 text-xl xs:text-2xl" />
-                        </div>
-                        <div className="burger-menu" onClick={updateMenuProjects}>
-                            <div className={burgerClassProjects}></div>
-                            <div className={burgerClassProjects}></div>
-                            <div className={burgerClassProjects}></div>
-                        </div>
-                    </nav>
-
-                    <div className={menuClassProjects}>
-                        <a href="https://mitchellgruner.com#about" className="block text-xl xs:text-2xl" onClick={updateMenuProjects}>About</a>
-                        <a href="https://mitchellgruner.com#projects-section" className="block text-xl xs:text-2xl" onClick={updateMenuProjects}>Projects</a>
-                        <a href="https://mitchellgruner.com#contact-section" className="block text-xl xs:text-2xl" onClick={updateMenuProjects}>Contact</a>
-                    </div>
-                </div>
-                <div className="2xl:max-w-screen-2xl m-auto desktop-nav">
-                    <nav className="h-20 flex justify-between p-6 md:px-8 lg:px-10 xl:px-16 2xl:px-14">
-                        <div className="flex items-center">
-                            <h1 className="mr-2 md:mr-4"><a href="https://mitchellgruner.com" className="block text-xl md:text-2xl 2xl:text-3xl" onClick={updateMenuProjects}>Mitchell Gruner</a></h1>
-                            <SiCodeberg className="icon text-xl md:text-2xl 2xl:text-3xl" />
-                        </div>
-                        <ul className="flex items-center">
-                            <NavLinksProjects link="" title="About" />
-                            <NavLinksProjects link="projects-section" title="Projects" />
-                            <NavLinksProjects link="contact-section" title="Contact" />
-                            <NavLinksProjects link="blog" title="Blog" />
                         </ul>
                     </nav>
                 </div>
