d0159e42dde515fb87a9733e715a1f7eaeb01787
Mitchell Gruner <mit.gruner@gmail.com>
2025-01-11 18:59:18 -0600

    initial commit of /src/pages/blog.tsx

---
+++ src/pages/blog.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Blog = () => {
+    return (
+        <div>
+            <h1>Blog Page</h1>
+            <p>Welcome to the blog page!</p>
+        </div>
+    );
+};
+
+export default Blog;
