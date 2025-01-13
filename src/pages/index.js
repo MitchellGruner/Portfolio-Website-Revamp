a103df53ee535f3972b9636269e1b8225dee38a3
Mitchell Gruner <mit.gruner@gmail.com>
2025-01-13 17:52:51 -0600

    remove 'bg-image' declaration

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,7 +57,7 @@ const Home = (props) => {
       </Head>
 
       <About />
-      <div className="2xl:max-w-screen-2xl m-auto pt-24 bg-image">
+      <div className="2xl:max-w-screen-2xl m-auto pt-24">
         <Projects productDetails={props} />
         <WorkProjects productDetails={props} />
         <ChakraProvider>
