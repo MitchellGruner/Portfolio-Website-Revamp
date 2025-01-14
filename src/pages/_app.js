15903b82b30a93877e08f4ccb4c231b8a0723fd8
Mitchell Gruner <mit.gruner@gmail.com>
2025-01-13 19:08:58 -0600

    import '../styles/Blog.scss'

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,7 @@ import '../styles/Navbar.scss?v=3';
 import '../styles/Projects.scss?v=2';
 import '../styles/ProjectsInfo.scss?v=2';
 import '../styles/Contact.scss?v=4';
+import '../styles/Blog.scss';
 
 export default function App({ Component, pageProps }) {
   return (
